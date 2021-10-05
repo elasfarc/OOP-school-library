717dcc6dfb9c4812716e55677dec60c4d966c735
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-10-05 05:08:14 +0200

    Add Teacher #can_use_services?

--- Teacher.rb
+++ Teacher.rb
@@ -5,4 +5,14 @@ class Teacher < Person
     super(age: age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
+  def can_use_services?
+    true
+  end
 end
+
+
+
+
+
+pp x = Teacher.new(age: 5, specialization: 'X', name: 'ali')
+pp x.can_use_services?
