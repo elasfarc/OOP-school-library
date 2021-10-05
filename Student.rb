f652bd777cdc5d4fc45cedf0bd85952800a4a7a9
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-10-05 05:03:49 +0200

    Add #play_hooky

--- Student.rb
+++ Student.rb
@@ -5,8 +5,10 @@ class Student < Person
     super(age: age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+
+
 end
-
-
-
-
