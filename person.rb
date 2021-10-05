3ed87ab92654de0fa674ed0501216fb0e5758bf2
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-10-05 07:10:45 +0200

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 class Person
   def initialize(age:, name: 'Unknown', parent_permission: true)
-    @id = rand()
+    @id = rand
     @name = name
     @age = age
     @parent_permission = parent_permission
