c90345a742a415a21300addd425d23a52e7e2d17
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-10-05 04:36:28 +0200

    Create Person Class with setters instance variables

---
+++ Person.rb
@@ -0,0 +1,13 @@
+class Person
+  def initialize(name:'Unknown', age:, parent_permission: true)
+    @id
+    @name = name
+    @age = age
+  end
+
+  attr_reader :id
+  attr_accessor :name, :age
+
+
+end
+
