65c318fc4fa8a7311e20abe8783050480e582bbf
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-10-05 07:08:55 +0200

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 class Person
   def initialize(age:, name: 'Unknown', parent_permission: true)
-    @id = nil
+    @id = rand()
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -10,7 +10,7 @@ class Person
   attr_accessor :name, :age
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
   private
