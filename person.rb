7833dac88996bce264d434a45d6457f717357c56
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-10-06 06:12:08 +0200

    Add Person #validate_name

--- person.rb
+++ person.rb
@@ -1,9 +1,12 @@
+require_relative './corrector'
+
 class Person
   def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = rand
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new(name: @name)
   end
 
   attr_reader :id
@@ -11,6 +14,10 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def validate_name
+    @name = @corrector.correct_name
   end
 
   private
