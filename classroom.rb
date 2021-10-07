d9bed9ae4b6042af3ce5ac798ba3b95385386e92
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-10-07 09:52:29 +0200

    Add association between Classroom and Student

--- classroom.rb
+++ classroom.rb
@@ -1,4 +1,4 @@
-class Classroom 
+class Classroom
   attr_accessor :label
   attr_reader :students
 
@@ -7,5 +7,8 @@ class Classroom
     @students = []
   end
 
-
+  def add_student(student)
+    @students.push(student)
+    student.classroom = self
+  end
 end
