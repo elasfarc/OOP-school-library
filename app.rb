e40186b762040da7f8a5fa3c67fe1e733206c436
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-10-08 13:27:02 +0200

    Add App #adding_rental_helper

--- app.rb
+++ app.rb
@@ -33,12 +33,13 @@ class App
                                      parent_permission: parent_permission)
   end
 
-  def collect_person_data(selection)
-    person_data = { student: %w[name age classroom parent_permission],
-                    teacher: %w[name age specialize] }
+  def collect_data(selection)
+    req_data = { student: %w[name age classroom parent_permission],
+                 teacher: %w[name age specialize],
+                 book: %w[title author] }
 
     args = {}
-    person_data[selection].each do |prop|
+    req_data[selection].each do |prop|
       print "#{prop}: "
       args[prop.to_sym] = gets.chomp
     end
@@ -47,7 +48,21 @@ class App
 
   def add_person
     print 'Do you want to create a student(1) or a teacher (2) '
-    gets.chomp == '1' ? add_student(collect_person_data(:student)) : add_teacher(collect_person_data(:teacher))
+    gets.chomp == '1' ? add_student(collect_data(:student)) : add_teacher(collect_data(:teacher))
+  end
+
+  def adding_rental_helper
+    puts 'Select a book from the following list by a number'
+    list_books.each_with_index { |book, i| puts "#{i}) #{book}" }
+    book = @state[:books][gets.chomp.to_i]
+    puts 'Select a person from the following list by a number (not ID) '
+    list_people.each_with_index { |person, i| puts "#{i}) #{person}" }
+    people = @state[:students] + @state[:teachers]
+    person = people[gets.chomp.to_i]
+    puts 'Date'
+    date = gets.chomp
+    Rental.new(date: date, person: person, book: book)
+    puts "\n ****rental creaded!*****"
   end
 
   def list_person_rentals(id)
@@ -68,5 +83,32 @@ class App
     puts '7 - Exit'
   end
 
-
+  def run
+    puts 'Welcome to School Library App!'
+    running = true
+    while running
+      app_ui
+      user_selection = gets.chomp.to_i
+      case user_selection
+      when 1
+        puts list_books
+      when 2
+        puts list_people
+      when 3
+        add_person
+      when 4
+        add_book(collect_data(:book))
+      when 5
+        adding_rental_helper
+      when 6
+        print 'ID of the person: '
+        list_person_rentals(gets.chomp.to_i)
+      when 7
+        running = false
+      else
+        puts 'Invalid selection, try again!'
+      end
+    end
+  end
 end
+
