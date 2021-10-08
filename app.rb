c179fd599750d62f520610cea23c37cb95f65086
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-10-08 10:42:54 +0200

    Add App #list_people -> returns array of all people in the state

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
+require_relative './student'
+
 class App
   attr_reader :state
 
   def initialize
     @state = { students: [], teachers: [], books: [] }
+  end
+
+  def list_people
+    people = @state[:students] + @state[:teachers]
+    people.map { |person| "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age} " }
   end
 end
