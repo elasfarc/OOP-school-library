174626f5faf73336e494f4e6414e61827ef86cc5
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-10-08 10:47:09 +0200

    Add App #list_books

--- app.rb
+++ app.rb
@@ -11,4 +11,17 @@ class App
     people = @state[:students] + @state[:teachers]
     people.map { |person| "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age} " }
   end
+
+  def list_books
+    @state[:books].map { |book| "Title: #{book.title}, Author: #{book.author}" }
+  end
 end
+
+# x = App.new
+# # pp x.state[:students]
+
+# student1 = Student.new(age: 12, classroom: 'x', name: 'Al', parent_permission: true)
+# puts student1
+# # x.state[:students] << student1
+
+# # puts x.list_people
